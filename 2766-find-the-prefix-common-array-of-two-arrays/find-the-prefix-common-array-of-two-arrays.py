ceb9ffa60c0b35f4803296e1f19c95f8b3abf900
MANNEM DURGA DARSAN <mannemdarsan@gmail.com>
2026-05-20 09:48:49 +0530

    Time: 126 ms (12.35%) | Memory: 12.4 MB (60.49%) - LeetSync

---
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def findThePrefixCommonArray(self, A, B):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :rtype: List[int]
+        """
+        C =[]
+        
+        for i in range (0,len(A)):
+            count = 0
+            for j in range (0,i+1):
+                if A[j] in B[:i+1]:
+                    count += 1
+            C.append(count)
+        return C 
