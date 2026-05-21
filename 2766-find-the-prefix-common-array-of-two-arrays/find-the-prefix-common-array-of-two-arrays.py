2b2063da9174016e8d57b66ece59d0ee882da00c
MANNEM DURGA DARSAN <mannemdarsan@gmail.com>
2026-05-21 22:03:54 +0530

    Update find-the-prefix-common-array-of-two-arrays.py

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
@@ -2,7 +2,7 @@ class Solution(object):
     def findThePrefixCommonArray(self, A, B):
         C =[]
         
-        for i in range (0,len(A)):
+        for i in range (0,len(A)): 
             count = 0
 
             for j in range (0,i+1):
