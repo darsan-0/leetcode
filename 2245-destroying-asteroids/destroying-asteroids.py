e99d06ca35ab90c1ed798e9da0acdd2ba84a6cd0
MANNEM DURGA DARSAN <mannemdarsan@gmail.com>
2026-05-31 09:31:59 +0530

    Time: 107 ms (48.28%) | Memory: 21.3 MB (72.41%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.py
@@ -0,0 +1,11 @@
+class Solution:
+    def asteroidsDestroyed(self, mass, asteroids):
+        asteroids.sort()
+
+        for asteroid in asteroids:
+            if mass < asteroid:
+                return False
+
+            mass += asteroid
+
+        return True
